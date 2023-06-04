45732c0c69312303df34e459701d6288bbe23b6b
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-04 19:14:09 +0100

    2018 day 1 part 2

--- 2018/1/index.test.js
+++ 2018/1/index.test.js
@@ -5,6 +5,12 @@ const input1 = fs.readFileSync(__dirname + "/input-1.txt").toString();
 const input2 = fs.readFileSync(__dirname + "/input-2.txt").toString();
 const input3 = fs.readFileSync(__dirname + "/input-3.txt").toString();
 const input4 = fs.readFileSync(__dirname + "/input-4.txt").toString();
+
+const input21 = fs.readFileSync(__dirname + "/input-2-1.txt").toString();
+const input22 = fs.readFileSync(__dirname + "/input-2-2.txt").toString();
+const input23 = fs.readFileSync(__dirname + "/input-2-3.txt").toString();
+const input24 = fs.readFileSync(__dirname + "/input-2-4.txt").toString();
+
 const challengeInput = fs.readFileSync(__dirname + "/input.txt").toString();
 
 test("algo", () => {
@@ -14,4 +20,13 @@ test("algo", () => {
   expect(algo(input4)).toBe(-6);
 
   console.log(algo(challengeInput));
+});
+
+test("algo part 2", () => {
+  expect(algo(input21, 2)).toBe(0);
+  expect(algo(input22, 2)).toBe(10);
+  expect(algo(input23, 2)).toBe(5);
+  expect(algo(input24, 2)).toBe(14);
+
+  console.log(algo(challengeInput, 2));
 });
--- 2018/1/index.ts
+++ 2018/1/index.ts
@@ -1,5 +1,27 @@
-export function algo(input: string) {
+export function algo(input: string, part: 1 | 2 = 1) {
   const changes: number[] = input.split(/\r?\n/).map((i) => Number(i));
 
-  return changes.reduce((carry, item) => carry += item, 0);
+  // Handle extra 0 at the handle
+  changes.pop();
+
+  if (1 === part) {
+    return changes.reduce((carry, item) => carry += item, 0);
+  }
+
+  let current = 0;
+  const frequencies = [0];
+
+  for (let i = 0; i < changes.length; i++) {
+    current += changes[i];
+    
+    if (frequencies.includes(current)) {
+      return current;
+    }
+
+    frequencies.push(current);
+
+    if (i === changes.length - 1) {
+      i = -1;
+    }
+  }
 }
