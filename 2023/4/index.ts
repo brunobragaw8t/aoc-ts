67b7a28a786d5729f696d5f1d7022dea74d70f59
Bruno Braga <brunobragaw8t@gmail.com>
2023-12-04 21:40:54 +0000

    2023 day 4 part 2

--- 2023/4/index.ts
+++ 2023/4/index.ts
@@ -1,3 +1,5 @@
+import { match } from "assert";
+
 export function algo(input: string): number {
   const cards = input.split('\n');
 
@@ -32,4 +34,44 @@ export function algo(input: string): number {
   }
 
   return total;
+}
+
+export function algo2(input: string): number {
+  const cards = input.split('\n');
+
+  const cardsCopies: Record<string, number> = {};
+
+  for (let i = 1; i <= cards.length; i++) {
+    cardsCopies[i] = 1;
+  }
+
+  for (let i = 1; i <= cards.length; i++) {
+    let matchingNumbers = 0;
+
+    const [winningNumbers, numbersGot] = cards[i - 1]
+      .replace(/^Card\s+\d+: /, '')
+      .split(' | ');
+
+    const winningNumbersArr = winningNumbers
+      .split(/\s+/)
+      .filter((n) => '' !== n);
+
+    const numbersGotArr = numbersGot
+      .split(/\s+/)
+      .filter((n) => '' !== n);
+
+    for (const nr of numbersGotArr) {
+      if (winningNumbersArr.includes(nr)) {
+        matchingNumbers++;
+      }
+    }
+
+    for (let j = 0; j < cardsCopies[i]; j++) {
+      for (let k = 1; k <= matchingNumbers; k++) {
+        cardsCopies[i + k]++;
+      }
+    }
+  }
+
+  return Object.entries(cardsCopies).reduce((acc, [_, v]) => acc += v, 0);
 }
