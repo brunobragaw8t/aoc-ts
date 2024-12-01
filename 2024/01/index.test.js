ce0386ea077204c478912ec549420be4590667d2
Bruno Braga <brunobragaw8t@gmail.com>
2024-12-01 11:05:09 +0000

    2024 day 1 part 2

--- 2024/01/index.test.js
+++ 2024/01/index.test.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
-const { algo } = require("./index.js");
+const { algo, algo2 } = require("./index.js");
 
 const inputDemo = fs.readFileSync(__dirname + "/input-demo-1.txt").toString();
 const inputChallenge = fs.readFileSync(__dirname + "/input.txt").toString();
 
 test("algo", () => {
   expect(algo(inputDemo)).toBe(11);
+  expect(algo2(inputDemo)).toBe(31);
   console.log('Result: ', algo(inputChallenge));
+  console.log('Result 2: ', algo2(inputChallenge));
 });
 
--- 2024/01/index.ts
+++ 2024/01/index.ts
@@ -21,3 +21,25 @@ export function algo(input: string): number {
 
   return sum;
 }
+
+export function algo2(input: string): number {
+  const lists: Record<string, number[]> = {
+    left: [],
+    right: [],
+  };
+
+  for (const row of input.split('\n').slice(0, -1)) {
+    const [left, right] = row.split('   ');
+    lists.left.push(Number(left));
+    lists.right.push(Number(right));
+  }
+
+  let score = 0;
+
+  for (const l of lists.left) {
+    const numberOfTimesInRight = lists.right.filter((r) => l === r).length;
+    score += l * numberOfTimesInRight;
+  }
+
+  return score;
+}
