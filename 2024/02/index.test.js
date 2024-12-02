55a0d05e109ba7a2949f2ee821ec0e839fa63c3e
Bruno Braga <brunobragaw8t@gmail.com>
2024-12-02 19:20:30 +0000

    2024 day 2 part 2

--- 2024/02/index.test.js
+++ 2024/02/index.test.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
-const { algo } = require("./index.js");
+const { algo, algo2 } = require("./index.js");
 
 const inputDemo = fs.readFileSync(__dirname + "/input-demo-1.txt").toString();
 const inputChallenge = fs.readFileSync(__dirname + "/input.txt").toString();
 
 test("algo", () => {
   expect(algo(inputDemo)).toBe(2);
+  expect(algo2(inputDemo)).toBe(4);
   console.log('Result: ', algo(inputChallenge));
+  console.log('Result 2: ', algo2(inputChallenge));
 });
--- 2024/02/index.ts
+++ 2024/02/index.ts
@@ -39,3 +39,36 @@ export function algo(input: string): number {
 
   return safe;
 }
+
+export function algo2(input: string): number {
+  let safe = 0;
+
+  rows: for (const row of input.split('\n').slice(0, -1)) {
+    const levels = row.split(' ').map((l) => Number(l));
+
+    const levelsGroup = [];
+
+    for (let i = 0; i < levels.length; i++) {
+      const tmp = [...levels];
+      tmp.splice(i, 1);
+      levelsGroup.push(tmp);
+    }
+
+    group: for (const levels of levelsGroup) {
+      if (isIncreasing(levels) || isDecreasing(levels)) {
+        for (let i = 1; i < levels.length; i++) {
+          const diff = Math.abs(levels[i] - levels[i - 1]);
+
+          if (diff < 1 || diff > 3) {
+            continue group;
+          }
+        }
+
+        safe++;
+        continue rows;
+      }
+    }
+  }
+
+  return safe;
+}
