6d77ead1f76eee677bfe9b0b159da96261c7326f
Bruno Braga <brunobragaw8t@gmail.com>
2024-12-07 14:35:29 +0000

    2024 day 5 part 2

--- 2024/05/index.test.js
+++ 2024/05/index.test.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { algo } = require("./index.js");
+const { algo, algo2 } = require("./index.js");
 
 const inputDemo = fs.readFileSync(__dirname + "/input-demo-1.txt").toString();
 const inputChallenge = fs.readFileSync(__dirname + "/input.txt").toString();
@@ -7,4 +7,6 @@ const inputChallenge = fs.readFileSync(__dirname + "/input.txt").toString();
 test("algo", () => {
   expect(algo(inputDemo)).toBe(143);
   console.log('Result: ', algo(inputChallenge));
+  expect(algo2(inputDemo)).toBe(123);
+  console.log('Result 2: ', algo2(inputChallenge));
 });
--- 2024/05/index.ts
+++ 2024/05/index.ts
@@ -30,13 +30,9 @@ export function algo(input: string): number {
     });
 
   updatesLoop: for (const update of updates) {
-    console.log('Update: ', update);
-
     const relevantRules = getRelevantRules(update, rules);
 
     for (const rule of relevantRules) {
-      console.log('Rule: ', rule);
-
       let foundFirst = false;
 
       for (const page of update) {
@@ -47,6 +43,87 @@ export function algo(input: string): number {
         }
 
         if (page === rule.x) foundFirst = true;
+      }
+    }
+
+    sum += update[Math.floor(update.length / 2)];
+  }
+
+  return sum;
+}
+
+/**
+ * Part 2
+ */
+
+function isCorrectlyOrdered(update: number[], rules: Rule[]): boolean {
+  for (const rule of rules) {
+    let foundFirst = false;
+
+    for (const page of update) {
+      // If second was found but we haven't found the first yet
+      if (page === rule.y && !foundFirst) {
+        return false
+      }
+
+      if (page === rule.x) foundFirst = true;
+    }
+  }
+
+  return true;
+}
+
+function obeysRules(page: number, update: number[], rules: Rule[]): boolean {
+  const pageIndex = update.indexOf(page);
+
+  const rulesRelevantToPage = rules.filter((r) => r.x === page);
+
+  for (const rule of rulesRelevantToPage) {
+    const compareIndex = update.indexOf(rule.y);
+
+    if (pageIndex > compareIndex) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function algo2(input: string): number {
+  let sum = 0;
+
+  const [rulesInput, updatesInput] = input.split('\n\n');
+
+  const rules: Rule[] = rulesInput
+    .split('\n')
+    .map((r) => {
+      const [x, y] = r.split('|');
+      return { x: Number(x), y: Number(y) };
+    });
+
+  const updates = updatesInput
+    .split('\n')
+    .slice(0, -1)
+    .map((u) => {
+      return u
+        .split(',')
+        .map((p) => Number(p));
+    });
+
+  for (const update of updates) {
+    const rulesRelevantToUpdate = getRelevantRules(update, rules);
+
+    if (isCorrectlyOrdered(update, rulesRelevantToUpdate)) {
+      continue;
+    }
+
+    while (!isCorrectlyOrdered(update, rulesRelevantToUpdate)) {
+      for (let i = 0; i < update.length; i++) {
+        if (!obeysRules(update[i], update, rulesRelevantToUpdate)) {
+          const tmp = update[i - 1];
+          update[i - 1] = update[i];
+          update[i] = tmp;
+        }
       }
     }
 
