7ea1b534a4b94a6970291244108e9729d2e305e1
Bruno Braga <brunobragaw8t@gmail.com>
2024-12-10 21:55:23 +0000

    2024 day 8 part 2

--- 2024/08/index.test.js
+++ 2024/08/index.test.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
-const { algo } = require("./index.js");
+const { algo, algo2 } = require("./index.js");
 
 const inputDemo = fs.readFileSync(__dirname + "/input-demo-1.txt").toString();
+const inputDemo2 = fs.readFileSync(__dirname + "/input-demo-2.txt").toString();
 const inputChallenge = fs.readFileSync(__dirname + "/input.txt").toString();
 
 test("algo", () => {
   expect(algo(inputDemo)).toBe(14);
   console.log('Result: ', algo(inputChallenge));
+});
+
+test("algo2", () => {
+  expect(algo2(inputDemo2)).toBe(9);
+  expect(algo2(inputDemo)).toBe(34);
+  console.log('Result2: ', algo2(inputChallenge));
 });
--- 2024/08/index.ts
+++ 2024/08/index.ts
@@ -84,3 +84,143 @@ export function algo(input: string): number {
 
   return antinodes.length;
 }
+
+export function algo2(input: string): number {
+  const map = input
+    .split('\n')
+    .slice(0, -1)
+    .map((r) => r.split(''));
+
+  const yLimit = map.length;
+  const xLimit = map[0].length;
+
+  const antennas: { frequency: string, x: number, y: number }[] = [];
+  const antinodes: string[] = [];
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] !== '.') {
+        antennas.push({ frequency: map[y][x], x, y });
+      }
+    }
+  }
+
+  for (const antenna of antennas) {
+    const sameFrequencyAntennas = antennas.filter((a) => {
+      return (
+        a.frequency === antenna.frequency
+        && a.x !== antenna.x
+        && a.y !== antenna.y
+      );
+    });
+
+    for (const sameFrequencyAntenna of sameFrequencyAntennas) {
+      addAntinode(antinodes, xLimit, yLimit, antenna.x, antenna.y);
+
+      const xDiff = Math.abs(antenna.x - sameFrequencyAntenna.x);
+      const yDiff = Math.abs(antenna.y - sameFrequencyAntenna.y);
+
+      if (
+        antenna.x >= sameFrequencyAntenna.x
+        && antenna.y >= sameFrequencyAntenna.y
+      ) {
+        let xState = antenna.x + xDiff;
+        let yState = antenna.y + yDiff;
+
+        while (xState < xLimit && yState < yLimit) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState += xDiff;
+          yState += yDiff;
+        }
+
+        xState = antenna.x - xDiff;
+        yState = antenna.y - yDiff;
+
+        while (xState >= 0 && yState >= 0) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState -= xDiff;
+          yState -= yDiff;
+        }
+
+        continue;
+      }
+
+      if (
+        antenna.x >= sameFrequencyAntenna.x
+        && antenna.y <= sameFrequencyAntenna.y
+      ) {
+        let xState = antenna.x + xDiff;
+        let yState = antenna.y - yDiff;
+
+        while (xState < xLimit && yState >= 0) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState += xDiff;
+          yState -= yDiff;
+        }
+
+        xState = antenna.x - xDiff;
+        yState = antenna.y + yDiff;
+
+        while (xState >= 0 && yState < yLimit) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState -= xDiff;
+          yState += yDiff;
+        }
+
+        continue;
+      }
+
+      if (
+        antenna.x <= sameFrequencyAntenna.x
+        && antenna.y <= sameFrequencyAntenna.y
+      ) {
+        let xState = antenna.x - xDiff;
+        let yState = antenna.y - yDiff;
+
+        while (xState >= 0 && yState >= 0) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState -= xDiff;
+          yState -= yDiff;
+        }
+
+        xState = antenna.x + xDiff;
+        yState = antenna.y + yDiff;
+
+        while (xState < xLimit && yState < yLimit) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState += xDiff;
+          yState += yDiff;
+        }
+
+        continue;
+      }
+
+      if (
+        antenna.x <= sameFrequencyAntenna.x
+        && antenna.y >= sameFrequencyAntenna.y
+      ) {
+        let xState = antenna.x - xDiff;
+        let yState = antenna.y + yDiff;
+
+        while (xState >= 0 && yState < yLimit) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState -= xDiff;
+          yState += yDiff;
+        }
+
+        xState = antenna.x + xDiff;
+        yState = antenna.y - yDiff;
+
+        while (xState < xLimit && yState >= 0) {
+          addAntinode(antinodes, xLimit, yLimit, xState, yState);
+          xState += xDiff;
+          yState -= yDiff;
+        }
+
+        continue;
+      }
+    }
+  }
+
+  return antinodes.length;
+}
