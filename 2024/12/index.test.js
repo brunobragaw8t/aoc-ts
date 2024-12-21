c67e794563035856c6d2efb1f454937107d912a8
Bruno Braga <brunobragaw8t@gmail.com>
2024-12-21 22:24:03 +0000

    2024 day 12 part 2

--- 2024/12/index.test.js
+++ 2024/12/index.test.js
@@ -1,18 +1,25 @@
 const fs = require("fs");
-const { algo } = require("./index.js");
+const { algo, algo2 } = require("./index.js");
 
 const inputDemo1 = fs.readFileSync(__dirname + "/input-demo-1.txt").toString();
 const inputDemo2 = fs.readFileSync(__dirname + "/input-demo-2.txt").toString();
 const inputDemo3 = fs.readFileSync(__dirname + "/input-demo-3.txt").toString();
+const inputDemo4 = fs.readFileSync(__dirname + "/input-demo-4.txt").toString();
+const inputDemo5 = fs.readFileSync(__dirname + "/input-demo-5.txt").toString();
 const inputChallenge = fs.readFileSync(__dirname + "/input.txt").toString();
 
 test("algo", () => {
   expect(algo(inputDemo1)).toBe(140);
   expect(algo(inputDemo2)).toBe(772);
   expect(algo(inputDemo3)).toBe(1930);
-  console.log('Result: ', algo(inputChallenge));
+  expect(algo(inputChallenge)).toBe(1483212);
 });
 
-// test("algo2", () => {
-//   console.log('Result 2: ', algo(inputChallenge));
-// });
+test("algo2", () => {
+  expect(algo2(inputDemo1)).toBe(80);
+  expect(algo2(inputDemo2)).toBe(436);
+  expect(algo2(inputDemo3)).toBe(1206);
+  expect(algo2(inputDemo4)).toBe(236);
+  expect(algo2(inputDemo5)).toBe(368);
+  console.log('Result 2: ', algo2(inputChallenge));
+});
--- 2024/12/index.ts
+++ 2024/12/index.ts
@@ -64,15 +64,10 @@ function getArea(coordinate: Coordinate, map: string[][], scannedCoordinates: st
   return area;
 }
 
-export function algo(input: string): number {
-  const map = input
-    .split('\n')
-    .slice(0, -1)
-    .map((r) => r.split(''));
-
+function getAreas(map: string[][]): Coordinate[][] {
   const scannedCoordinates: string[] = [];
 
-  const areas: Coordinate[][] = []
+  const areas: Coordinate[][] = [];
 
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
@@ -89,6 +84,17 @@ export function algo(input: string): number {
       areas.push(getArea({ typeOfPlant, x, y }, map, scannedCoordinates));
     }
   }
+
+  return areas;
+}
+
+export function algo(input: string): number {
+  const map = input
+    .split('\n')
+    .slice(0, -1)
+    .map((r) => r.split(''));
+
+  const areas = getAreas(map);
 
   // console.log('Areas:');
   // console.dir(areas, { colors: true, depth: Infinity });
@@ -131,6 +137,160 @@ export function algo(input: string): number {
     price += area.length * perimeter;
 
     // console.log(`Perimeter of ${area[0].typeOfPlant} is ${perimeter}`);
+  }
+
+  return price;
+}
+
+/**
+ * Part 2
+ */
+
+type Fence = {
+  side: 'top' | 'right' | 'bottom' | 'left';
+  x: number;
+  y: number;
+};
+
+function getSides(map: string[][], fences: Fence[]): number {
+  const scannedFences: string[] = [];
+  let sides = 0;
+  console.log('Fences:');
+  console.dir(fences, { colors: true, depth: Infinity });
+
+  for (const fence of fences) {
+    const fenceCoordinateString = `${fence.side}|${fence.x}|${fence.y}`;
+
+    if (scannedFences.includes(fenceCoordinateString)) {
+      continue;
+    }
+
+    sides++;
+    scannedFences.push(fenceCoordinateString);
+
+    if (fence.side === 'top' || fence.side === 'bottom') {
+      for (let x = fence.x + 1; x <= map[0].length; x++) {
+        const isItAFence = fences.find((f) => {
+          return (
+            f.side === fence.side
+            && f.x === x
+            && f.y === fence.y
+          );
+        });
+
+        if (typeof isItAFence === 'undefined') {
+          break;
+        }
+
+        scannedFences.push(`${fence.side}|${x}|${fence.y}`);
+      }
+
+      for (let x = fence.x - 1; x >= -1; x--) {
+        const isItAFence = fences.find((f) => {
+          return (
+            f.side === fence.side
+            && f.x === x
+            && f.y === fence.y
+          );
+        });
+
+        if (typeof isItAFence === 'undefined') {
+          break;
+        }
+
+        scannedFences.push(`${fence.side}|${x}|${fence.y}`);
+      }
+    }
+
+    if (fence.side === 'right' || fence.side === 'left') {
+      for (let y = fence.y + 1; y <= map.length; y++) {
+        const isItAFence = fences.find((f) => {
+          return (
+            f.side === fence.side
+            && f.x === fence.x
+            && f.y === y
+          );
+        });
+
+        if (typeof isItAFence === 'undefined') {
+          break;
+        }
+
+        scannedFences.push(`${fence.side}|${fence.x}|${y}`);
+      }
+
+      for (let y = fence.y - 1; y >= -1; y--) {
+        const isItAFence = fences.find((f) => {
+          return (
+            f.side === fence.side
+            && f.x === fence.x
+            && f.y === y
+          );
+        });
+
+        if (typeof isItAFence === 'undefined') {
+          break;
+        }
+
+        scannedFences.push(`${fence.side}|${fence.x}|${y}`);
+      }
+    }
+  }
+
+  return sides;
+}
+
+export function algo2(input: string): number {
+  const map = input
+    .split('\n')
+    .slice(0, -1)
+    .map((r) => r.split(''));
+
+  const areas = getAreas(map);
+
+  // console.log('Areas:');
+  // console.dir(areas, { colors: true, depth: Infinity });
+
+  let price = 0;
+
+  for (const area of areas) {
+    const fences: Fence[] = [];
+
+    for (const coordinate of area) {
+      if (
+        coordinate.y - 1 < 0
+        || coordinate.typeOfPlant !== map[coordinate.y - 1][coordinate.x]
+      ) {
+        fences.push({ side: 'top', x: coordinate.x, y: coordinate.y - 1 });
+      }
+
+      if (
+        coordinate.x + 1 >= map[coordinate.y].length
+        || coordinate.typeOfPlant !== map[coordinate.y][coordinate.x + 1]
+      ) {
+        fences.push({ side: 'right', x: coordinate.x + 1, y: coordinate.y });
+      }
+
+      if (
+        coordinate.y + 1 >= map.length
+        || coordinate.typeOfPlant !== map[coordinate.y + 1][coordinate.x]
+      ) {
+        fences.push({ side: 'bottom', x: coordinate.x, y: coordinate.y + 1 });
+      }
+
+      if (
+        coordinate.x - 1 < 0
+        || coordinate.typeOfPlant !== map[coordinate.y][coordinate.x - 1]
+      ) {
+        fences.push({ side: 'left', x: coordinate.x - 1, y: coordinate.y });
+      }
+    }
+
+    const sides = getSides(map, fences);
+
+    price += area.length * sides;
+
+    console.log(`Area of ${area[0].typeOfPlant} has ${sides} sides`);
   }
 
   return price;
