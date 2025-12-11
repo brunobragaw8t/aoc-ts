075f7da4f70aae6ed60e32f1852f75f2f34d11c0
Bruno Braga <brunobragaw8t@gmail.com>
2025-12-11 17:28:13 +0000

    2025 day 3 part 2

--- 2025/03/index.test.js
+++ 2025/03/index.test.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
-const { algo } = require("./index.js");
+const { algo, algo2 } = require("./index.js");
 
 const inputDemo1 = fs.readFileSync(__dirname + "/input-demo-1.txt").toString();
 const input = fs.readFileSync(__dirname + "/input.txt").toString();
 
 test("algo", () => {
   expect(algo(inputDemo1)).toBe(357);
-  console.log(algo(input));
+  expect(algo(input)).toBe(17193);
+  expect(algo2(inputDemo1)).toBe(3121910778619);
+  console.log(algo2(input));
 });
--- 2025/03/index.ts
+++ 2025/03/index.ts
@@ -19,3 +19,48 @@ export function algo(input: string): number {
 
   return sum;
 }
+
+function getCombinations(value: string, bank: string): string[] {
+  if (bank.length === 0 && value.length !== 12) return [];
+
+  if (value.length === 12) return [`${value}`];
+
+  const combinations = [];
+  let currMaxValue = 0;
+
+  for (let i = 0; i < bank.length; i++) {
+    if (Number(bank[i]) <= currMaxValue) continue;
+    currMaxValue = Number(bank[i]);
+
+    combinations.push(
+      Math.max(
+        ...getCombinations(`${value}${bank[i]}`, bank.slice(i + 1)).map(Number),
+      ).toString(),
+    );
+  }
+
+  return combinations;
+}
+
+export function algo2(input: string): number {
+  const banks = input.trim().split("\n");
+
+  let sum = 0;
+  let i = 0;
+
+  for (const bank of banks) {
+    const combinations = getCombinations("", bank);
+
+    const max = Math.max(...combinations.map(Number));
+
+    sum += max;
+
+    console.log("i", i);
+    console.log("max", max);
+    console.log("sum", sum);
+
+    i++;
+  }
+
+  return sum;
+}
