fc68c0fd182f94dc75422e811bb997aec47a3add
Bruno Braga <brunobragaw8t@gmail.com>
2025-12-11 14:47:37 +0000

    2025 day 2 part 2

--- 2025/02/index.test.js
+++ 2025/02/index.test.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
-const { algo } = require("./index.js");
+const { algo, isInvalidId, isInvalidIdPart2 } = require("./index.js");
 
 const inputDemo1 = fs.readFileSync(__dirname + "/input-demo-1.txt").toString();
 const input = fs.readFileSync(__dirname + "/input.txt").toString();
 
 test("algo", () => {
-  expect(algo(inputDemo1)).toBe(1227775554);
-  console.log(algo(input));
+  expect(algo(inputDemo1, isInvalidId)).toBe(1227775554);
+  expect(algo(input, isInvalidId)).toBe(29940924880);
+  expect(algo(inputDemo1, isInvalidIdPart2)).toBe(4174379265);
+  console.log(algo(input, isInvalidIdPart2));
 });
--- 2025/02/index.ts
+++ 2025/02/index.ts
@@ -1,4 +1,4 @@
-function isInvalidId(id: number): boolean {
+export function isInvalidId(id: number): boolean {
   const string = id.toString();
   const length = string.length;
   const first = string.slice(0, length / 2);
@@ -6,7 +6,30 @@ function isInvalidId(id: number): boolean {
   return first === last;
 }
 
-export function algo(input: string): number {
+export function isInvalidIdPart2(id: number): boolean {
+  const string = id.toString();
+
+  for (let i = 1; i <= string.length / 2; i++) {
+    const firstValue = string.slice(0, i);
+
+    const values = [];
+
+    for (let j = i; j < string.length; j += i) {
+      values.push(string.slice(j, j + i));
+    }
+
+    if (values.every((value) => value === firstValue)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export function algo(
+  input: string,
+  isInvalidId: (id: number) => boolean,
+): number {
   const ranges = input.trim().split(",");
 
   let sum = 0;
